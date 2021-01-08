086a8af90deecd1a033ba16b032117b22b43098d
sabrina-fontana <sabfon@hotmail.it>
2021-01-08 18:12:09 +0100

    aggiunto max 84 tentativi

--- js/script.js
+++ js/script.js
@@ -17,8 +17,7 @@ var numeroRandom;
 var numeriComputer = [];
 var numeroScelto;
 var numeriUtente = [];
-var mina = false;
-var punteggio = document.getElementById('punteggio');
+var testoPunteggio = document.getElementById('punteggio');
 
 // IL COMPUTER DEVE GENERARE 16 NUMERI CASUALI DA 1 A 100 CHE NON POSSONO ESSERE DUPLICATI
 // creo un ciclo che si ripete fino a che l'array  ha 16 elementi e ogni volta pusho il numero randomico nell'array numeriComputer SOLO SE ancora non lo include
@@ -30,21 +29,21 @@ while (numeriComputer.length < 16) {
   }
 }
 console.log('I numeri randomici creati dal computer sono: ' + numeriComputer);
-// CHIEDO ALL'UTENTE N VOLTE DI INSERIRE UN NUMERO COMPRESO TRA 1 E 100; NON PUO' INSERIRE PIU' VOLTE LO STESSO NUMERO
+// CHIEDO ALL'UTENTE MAX 84 VOLTE DI INSERIRE UN NUMERO COMPRESO TRA 1 E 100; NON PUO' INSERIRE PIU' VOLTE LO STESSO NUMERO
 // il ciclo continua fino a che il numero inserito dall'utente corrisponde a uno dei numeri randomici creati dal computer
 
-while (!numeriComputer.includes(numeroScelto)) {
+while (!numeriComputer.includes(numeroScelto) && numeriUtente.length < 84) {
   numeroScelto = prompt('Inserisci un numero da 1 a 100');
   if (errorNumber(numeroScelto)) {
     alert('Inserisci un numero da 1 a 100');
   } else {
     numeroScelto = parseInt(numeroScelto);
-    if (noRepeatNumber(numeriUtente, numeroScelto)) {
+    // ricontrollo che il numero inserito dall'utente non corrisponda a uno dei numeri randomici
+    if (noRepeatNumber(numeriUtente, numeroScelto) && !numeriComputer.includes(numeroScelto)) {
       numeriUtente.push(numeroScelto);
     }
     console.log(numeroScelto);
   }
 }
 
-// length - 1 perchè l'ultimo valore pushato nell'array è quello errato che ha fatto terminare il gioco
-punteggio.innerHTML = 'Il tuo punteggio è ' + (numeriUtente.length - 1);
+testoPunteggio.innerHTML = 'Il tuo punteggio è ' + (numeriUtente.length);
