23ec5fd9147454e756dc5bc4b40ca26d8d209467
sabrina-fontana <sabfon@hotmail.it>
2021-01-08 16:58:19 +0100

    punteggio

--- js/script.js
+++ js/script.js
@@ -13,10 +13,10 @@ var numeroRandom;
 var numeriComputer = [];
 var numeroScelto;
 var numeriUtente = [];
-var incluso;
+var punteggio = document.getElementById('punteggio');
 
 // IL COMPUTER DEVE GENERARE 16 NUMERI CASUALI DA 1 A 100 CHE NON POSSONO ESSERE DUPLICATI
-// creo un ciclo che si ripete fino a che l'array ha 16 elementi e ogni volta pusho il numero randomico nell'array numeriComputer SOLO SE ancora non lo include
+// creo un ciclo che si ripete fino a che l'array  ha 16 elementi e ogni volta pusho il numero randomico nell'array numeriComputer SOLO SE ancora non lo include
 while (numeriComputer.length < 16) {
   numeroRandom = randomNumber(1, 100);
   // controllo che non ci siano numeri uguali nell'array
@@ -28,9 +28,16 @@ console.log(numeriComputer);
 // CHIEDO ALL'UTENTE N VOLTE DI INSERIRE UN NUMERO COMPRESO TRA 1 E 100; NON PUO' INSERIRE PIU' VOLTE LO STESSO NUMERO
 // il ciclo continua fino a che il numero inserito dall'utente corrisponde a uno dei numeri randomici creati dal computer
 while (!numeriComputer.includes(numeroScelto)) {
-  numeroScelto = parseInt(prompt('Inserisci un numero da 1 a 100'));
-  if (noRepeatNumber(numeriUtente, numeroScelto)) {
-    numeriUtente.push(numeroScelto);
+  numeroScelto = prompt('Inserisci un numero da 1 a 100');
+  if (isNaN(numeroScelto) || numeroScelto < 1 || numeroScelto > 100 || numeroScelto.length > 3) {
+    alert('Inserisci un numero da 1 a 100');
+  } else {
+    numeroScelto = parseInt(numeroScelto);
+    if (noRepeatNumber(numeriUtente, numeroScelto)) {
+      numeriUtente.push(numeroScelto);
+    }
+    console.log(numeroScelto);
   }
-  console.log(numeroScelto);
 }
+
+punteggio.innerHTML = 'Il tuo punteggio è ' + (numeriUtente.length - 1);
